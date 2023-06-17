70e6b7d514d4bf61b69889054f432294c10ab1ad
isaak <IJW123@users.noreply.github.com>
2023-06-17 20:36:32 +0200

    Added reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use std::collections::HashMap;
 
-fn main() {
-   println!("test");
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
 }
+
+ 
